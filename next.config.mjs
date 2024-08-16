95bd423c7f1ce6ab80651d6e9d52d638c168562e
Zhang Peng <onnttf@gmail.com>
2024-08-16 10:10:39 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/emoji",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
